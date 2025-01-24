9b44bfc240d5b95e2a18adb85848793c6e64683b
Chanbadsha <chanbadsha005@gmail.com>
2025-01-24 09:21:03 +0600

    implement login

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log(user);
+  return (
+    <div>
+      {/* Navigation Bar */}
+      <header className="bg-gray-800">
+        <nav className="flex items-center justify-between p-4 max-w-7xl mx-auto text-white">
+          {/* Header Logo */}
+          <div className="flex items-center">
+            <Image
+              src="/logo.jpg"
+              alt="BlogMania Logo"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <span className="ml-2 text-lg font-bold">BlogMania</span>
+          </div>
+
+          {/* Navigation Links */}
+          <ul className="hidden md:flex space-x-8">
+            <li>
+              <Link href="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className="hover:underline">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="/api/auth/login" className="hover:underline">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link href="/api/auth/register" className="hover:underline">
+                Register
+              </Link>
+            </li>
+          </ul>
+          {user ? (
+            <>
+              <h2>{user.given_name}</h2>
+            </>
+          ) : (
+            <>
+              <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded">
+                Get Started
+              </button>
+            </>
+          )}
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,62 +30,18 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Navigation Bar */}
-        <header className="bg-gray-800">
-          <nav className="flex items-center justify-between p-4 max-w-7xl mx-auto text-white">
-            {/* Header Logo */}
-            <div className="flex items-center">
-              <Image
-                src="/logo.jpg"
-                alt="YourAppName Logo"
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
-              <span className="ml-2 text-lg font-bold">BlogMania</span>
-            </div>
-
-            {/* Navigation Links */}
-            <ul className="hidden md:flex space-x-8">
-              <li>
-                <Link href="/" className="hover:underline">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link href="/blog" className="hover:underline">
-                  Blog
-                </Link>
-              </li>
-              <li>
-                <Link href="/login" className="hover:underline">
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link href="/register" className="hover:underline">
-                  Register
-                </Link>
-              </li>
-            </ul>
-
-            <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded">
-              Get Started
-            </button>
-          </nav>
-        </header>
-
+        <Navbar></Navbar>
         {/* Main Content */}
         <main className="min-h-screen bg-gray-100">{children}</main>
 
-        {/* Footer */}
+        {/* Footer
         <footer className="bg-gray-800 text-white py-4">
           <div className="max-w-7xl mx-auto text-center">
             <p>
               © {new Date().getFullYear()} YourAppName. All rights reserved.
             </p>
           </div>
-        </footer>
+        </footer> */}
       </body>
     </html>
   );
